06e849ebd979be9e0d0a67626b75badd0d23a2cb
Ben Sheffield <bshefb@gmail.com>
2019-01-25 22:45:28 +0000

    feat(robot): connect to test websocket server
    This connects to echo.websocket.org which just echos anything that it is sent. When we connect, we send "Connected" then we shoud get "get text: Connected" back.

--- robot/esp8266/esp8266.ino
+++ robot/esp8266/esp8266.ino
@@ -1,9 +1,39 @@
-#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <WebSocketsClient.h>
+#include <ESP8266WiFi.h>
 #include "wifi.h"
 
+char path[] = "/";
+char host[] = "echo.websocket.org";
+  
+WebSocketsClient socket;
+WiFiClient client;
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+	switch(type) {
+		case WStype_DISCONNECTED:
+      digitalWrite(D8, LOW);
+			Serial.printf("[WSc] Disconnected!\n");
+			break;
+		case WStype_CONNECTED: {
+      digitalWrite(D8, HIGH);
+			Serial.printf("[WSc] Connected to url: %s\n", payload);
+      socket.sendTXT("Connected");
+		}
+			break;
+		case WStype_TEXT:
+			Serial.printf("[WSc] get text: %s\n", payload);
+      break;
+		case WStype_BIN:
+			Serial.printf("[WSc] get binary length: %u\n", length);
+			hexdump(payload, length);
+      break;
+	}
+}
+
 void setup() {
-  // use pin 7 as our LED indicator
+  // use pin 7/8 as our LED indicators
   pinMode(D7, OUTPUT);
+  pinMode(D8, OUTPUT);
 
   // begin serial communications
   Serial.begin(9600);
@@ -34,7 +64,15 @@ void setup() {
   Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
 
   digitalWrite(D7, HIGH);
+
+  delay(5000);
+
+  // connect to websocket server
+  socket.begin("echo.websocket.org", 80, "/");
+  socket.onEvent(webSocketEvent);
+  socket.setReconnectInterval(5000);
 }
 
 void loop() { 
+  socket.loop();
 }
