c381476dce683aa422009618190642414b424615
Ben Sheffield <bshefb@gmail.com>
2019-01-25 18:34:15 +0000

    feat(robot server): read and echo back websocket messages

--- robotserver/go.mod
+++ robotserver/go.mod
@@ -1,1 +1,3 @@
 module github.com/mrbenshef/SmartHomeAdapters/robotserver
+
+require github.com/gorilla/websocket v1.4.0
--- robotserver/main.go
+++ robotserver/main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func pingHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("pong"))
+// upgrader upgrades http connections to WebSocket
+var upgrader = websocket.Upgrader{}
+
+// connectHandler establishes the WebSocket with the client
+func connectHandler(w http.ResponseWriter, r *http.Request) {
+	// upgrade request
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Failed to upgrade: ", err)
+		return
+	}
+	defer c.Close()
+
+	for {
+		// receive a message
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("Failed to read message: ", err)
+		}
+		log.Println("Received: ", message)
+
+		// echo it back
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("Failed to write message: ", err)
+			break
+		}
+	}
 }
 
 func main() {
 	// register routes
-	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/connect", connectHandler)
 
 	// start server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
