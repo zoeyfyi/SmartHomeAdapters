075a6145052d877aac5e9b893507d010fc7a709c
Ben Sheffield <bshefb@gmail.com>
2019-01-26 21:44:30 +0000

    test(user server): register route field validation

--- userserver/main_test.go
+++ userserver/main_test.go
@@ -1,27 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func TestPing(t *testing.T) {
-	req, err := http.NewRequest("GET", "/ping", nil)
-
-	if err != nil {
-		t.Errorf("Error: %v", err)
+func TestRegisterFieldValidation(t *testing.T) {
+	cases := []struct {
+		body           string
+		expectedStatus int
+		expectedError  string
+	}{
+		{"", http.StatusBadRequest, ErrorInvalidJSON},
+		{"{\"email\":\"\", \"password\":\"bar\"}", http.StatusBadRequest, ErrorEmailBlank},
+		{"{\"email\":\"foo@email.com\", \"password\":\"\"}", http.StatusBadRequest, ErrorPasswordBlank},
+		{"{\"email\":\"foo\", \"password\":\"bar\"}", http.StatusBadRequest, ErrorEmailInvalid},
 	}
 
-	rr := httptest.NewRecorder()
+	for _, c := range cases {
+		req, err := http.NewRequest("GET", "/register", strings.NewReader(c.body))
+		if err != nil {
+			t.Errorf("Error: %v", err)
+		}
 
-	http.HandlerFunc(pingHandler).ServeHTTP(rr, req)
+		rr := httptest.NewRecorder()
+		http.HandlerFunc(registerHandler).ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Status code differs. Expected \"%d\", Got \"%d\"", http.StatusOK, status)
+		if status := rr.Code; status != c.expectedStatus {
+			t.Errorf("Status code differs. Expected \"%d\", Got \"%d\"", c.expectedStatus, status)
+		}
+
+		var restError restError
+		err = json.NewDecoder(rr.Body).Decode(&restError)
+		if err != nil {
+			t.Errorf("Could not read error json: %v", err)
+		}
+
+		if restError.Error != c.expectedError {
+			t.Errorf("Error differs. Expected \"%s\", Got: \"%s\"", c.expectedError, restError)
+		}
 	}
 
-	if rr.Body.String() != "pong" {
-		t.Errorf("Body differs. Expected \"%s\", Got: \"%s\"", "pong", rr.Body.String())
-	}
 }
