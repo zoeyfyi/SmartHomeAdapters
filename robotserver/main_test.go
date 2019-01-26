f838dcffceae32e98792eeb062ddfe4e68c6ac0e
Ben Sheffield <bshefb@gmail.com>
2019-01-26 15:10:19 +0000

    test(robot server): connect to websocket

--- robotserver/main_test.go
+++ robotserver/main_test.go
@@ -3,25 +3,47 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/gorilla/websocket"
 )
 
-func TestPing(t *testing.T) {
-	req, err := http.NewRequest("GET", "/ping", nil)
+// httpToWsProtocal converts a http url to a WebSocket url
+func httpToWsProtocol(url string) string {
+	return "ws" + strings.TrimPrefix(url, "http")
+}
 
+type testHandler struct{ *testing.T }
+
+func (t testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	connectHandler(w, r)
+}
+
+type testServer struct {
+	Handler http.Handler
+	Server  *httptest.Server
+	URL     string
+}
+
+func newServer(t *testing.T) *testServer {
+	var server testServer
+	http.HandleFunc("/connect", connectHandler)
+	http.HandleFunc("/led/on", ledOnHandler)
+	http.HandleFunc("/led/off", ledOffHandler)
+	server.Handler = http.DefaultServeMux
+	server.Server = httptest.NewServer(server.Handler)
+	server.URL = httpToWsProtocol(server.Server.URL)
+	return &server
+}
+
+func TestConnectToWebSocket(t *testing.T) {
+	s := newServer(t)
+	defer s.Server.Close()
+
+	ws, _, err := websocket.DefaultDialer.Dial(s.URL+"/connect", nil)
 	if err != nil {
-		t.Errorf("Error: %v", err)
+		t.Fatalf("Error dialing: %v", err)
 	}
-
-	rr := httptest.NewRecorder()
-
-	http.HandlerFunc(pingHandler).ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Status code differs. Expected \"%d\", Got \"%d\"", http.StatusOK, status)
-	}
-
-	if rr.Body.String() != "pong" {
-		t.Errorf("Body differs. Expected \"%s\", Got: \"%s\"", "pong", rr.Body.String())
-	}
+	defer ws.Close()
 }
