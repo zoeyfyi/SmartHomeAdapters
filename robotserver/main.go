cf8106ba7267a59f00cdda0ef173ecb0d3ed9494
Ben Sheffield <bshefb@gmail.com>
2019-01-26 00:09:01 +0000

    feat(robot server): send led on/off message with http request
    /led/on turns the led on
    /led/off turns the led off

--- robotserver/main.go
+++ robotserver/main.go
@@ -8,38 +8,72 @@ import (
 )
 
 // upgrader upgrades http connections to WebSocket
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // ignore origin
+	},
+}
+
+// socket to send messages to
+var socket *websocket.Conn
 
 // connectHandler establishes the WebSocket with the client
 func connectHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Attempting to establish WebSocket connection")
+
 	// upgrade request
-	c, err := upgrader.Upgrade(w, r, nil)
+	if socket != nil {
+		log.Println("Closing existing socket")
+		socket.Close()
+	}
+
+	newSocket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Failed to upgrade: ", err)
 		return
 	}
-	defer c.Close()
 
-	for {
-		// receive a message
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("Failed to read message: ", err)
-		}
-		log.Println("Received: ", message)
+	log.Println("Socket opened")
+	socket = newSocket
+}
 
-		// echo it back
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("Failed to write message: ", err)
-			break
-		}
+func ledOnHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Turning the LED on")
+
+	if socket == nil {
+		log.Println("Robot not connected")
+		return
+	}
+
+	// send LED on message to robot
+	message := []byte("led on")
+	if err := socket.WriteMessage(websocket.TextMessage, message); err != nil {
+		log.Println("Failed to send LED on message")
+		return
+	}
+}
+
+func ledOffHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Turning the LED off")
+
+	if socket == nil {
+		log.Println("Robot not connected")
+		return
+	}
+
+	// send LED off message to robot
+	message := []byte("led off")
+	if err := socket.WriteMessage(websocket.TextMessage, message); err != nil {
+		log.Println("Failed to send LED off message")
+		return
 	}
 }
 
 func main() {
 	// register routes
 	http.HandleFunc("/connect", connectHandler)
+	http.HandleFunc("/led/on", ledOnHandler)
+	http.HandleFunc("/led/off", ledOffHandler)
 
 	// start server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
