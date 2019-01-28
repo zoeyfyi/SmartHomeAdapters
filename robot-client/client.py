e7a77161f706db03de7d7722af21f5d800b72d82
Ben Sheffield <bshefb@gmail.com>
2019-01-28 14:02:49 +0000

    feat(robot client): receive websocket messages and forward to serial

--- robot-client/client.py
+++ robot-client/client.py
@@ -1,11 +1,35 @@
 from time import sleep
 import serial
+
+import websocket
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
-on = 0
-while True:
-	if on == 1: 
-		on = 0 
+
+def on_open(ws):
+	print("Websocket connected")
+
+def on_error(ws, error):
+	print("Websocket error", error)
+
+def on_close(ws):
+	print("Websocket closed")
+
+def on_message(ws, msg):
+	if msg == "led on":
+		print("Sending 1")
+		ser.write(str(chr(1)).encode())
+	elif msg == "led off":
+		print("Sending 0")
+		ser.write(str(chr(0)).encode())
 	else:
-		on = 1
-	ser.write(str(chr(on)).encode())
-	sleep(.1)
+		print("Unknown message")
+
+ws = websocket.WebSocket()
+ws.on_open = on_open
+ws = websocket.WebSocketApp("ws://0.0.0.0:8080/connect",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+
+ws.run_forever()
+
