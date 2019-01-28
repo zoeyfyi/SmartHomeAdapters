99b7681f63d772819d957011807ce3529f864c15
Ben Sheffield <bshefb@gmail.com>
2019-01-28 04:17:13 +0000

    feat(robot): turn LED on/off over serial

--- robot/uno-r3/uno-r3.ino
+++ robot/uno-r3/uno-r3.ino
@@ -1,37 +1,23 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
 // the setup function runs once when you press reset or power the board
 void setup() {
+  // begin serial communications
+  Serial.begin(9600);
+  delay(10);
+  
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(3000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(3000);                       // wait for a second
+  if(Serial.available() > 0){
+      // turn on when we receive a 1, and off when we receive a 0
+      int value = Serial.read();
+      if(value == '1') {
+          digitalWrite(LED_BUILTIN, HIGH);
+      } else if (value == '0') {
+          digitalWrite(LED_BUILTIN, LOW);
+      }
+  }
 }
