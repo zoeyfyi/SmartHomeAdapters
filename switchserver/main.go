c30042237b607aca939734e6e7fc57948ebc87f5
Ben Sheffield <bshefb@gmail.com>
2019-01-29 02:45:45 +0000

    feat(switch server): dockerise and connect to postgres

--- switchserver/main.go
+++ switchserver/main.go
@@ -1,14 +1,67 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+// database connection infomation
+var (
+	username = os.Getenv("DB_USERNAME")
+	password = os.Getenv("DB_PASSWORD")
+	database = os.Getenv("DB_DATABASE")
+	url      = os.Getenv("DB_URL")
 )
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong"))
 }
 
+func connectionStr() string {
+	if username == "" {
+		username = "postgres"
+	}
+	if password == "" {
+		password = "password"
+	}
+	if url == "" {
+		url = "localhost:5432"
+	}
+	if database == "" {
+		database = "postgres"
+	}
+
+	return fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", username, password, url, database)
+}
+
+func getDb() *sql.DB {
+	log.Printf("Connecting to database with \"%s\"\n", connectionStr())
+	db, err := sql.Open("postgres", connectionStr())
+	if err != nil {
+		log.Fatalf("Failed to connect to postgres: %v", err)
+	}
+
+	return db
+}
+
 func main() {
+	// connect to database
+	db := getDb()
+	defer db.Close()
+
+	// test connection
+	err := db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping postgres: %v", err)
+	}
+
+	log.Printf("Connected to database: %+v\n", db.Stats())
+
 	// register routes
 	http.HandleFunc("/ping", pingHandler)
 
