9312d931ced2a4c719f2c212491dd8638e8feaf6
Ben Sheffield <bshefb@gmail.com>
2019-01-30 04:34:30 +0000

    docs(android): robot adapter

--- android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/RobotAdapter.kt
+++ android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/RobotAdapter.kt
@@ -12,7 +12,8 @@ import com.github.halspals.smarthomeadapters.smarthomeadapters.model.Robot
 
 class RobotAdapter (private val context: Context) :  BaseAdapter() {
 
-    private val robotIcons = listOf<Int>(
+    // TODO: remove once we have a real data source
+    private val robotIcons = listOf(
         R.drawable.basic_accelerator,
         R.drawable.basic_chronometer,
         R.drawable.basic_home,
@@ -22,17 +23,21 @@ class RobotAdapter (private val context: Context) :  BaseAdapter() {
         R.drawable.basic_lock_open
     )
 
+    // TODO: get list of robots from REST API
     private val robots = (1..20).map {
         Robot("Robot $it", robotIcons[it % robotIcons.size])
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        // inflate card view
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view: View = convertView ?: inflater.inflate(R.layout.view_robot_card, parent, false)
 
+        // get internal views
         val robotIcon = view.findViewById<ImageView>(R.id.robot_icon_image_view)
         val robotNickname = view.findViewById<TextView>(R.id.robot_nickname_text_view)
 
+        // configure views
         robotIcon.setImageResource(robots[position].iconDrawable)
         robotNickname.text = robots[position].nickname
 
