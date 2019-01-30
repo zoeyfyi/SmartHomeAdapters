fe9c0f1cda570479b0e1cdfc8f4ab18e91fd6f23
Ben Sheffield <bshefb@gmail.com>
2019-01-30 18:53:38 +0000

    feat(android): UI for connecting to adapter

---
+++ android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/ConnectToAdapterFragment.kt
@@ -0,0 +1,25 @@
+package com.github.halspals.smarthomeadapters.smarthomeadapters
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ConnectToAdapterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_connect_to_adapter, container, false)
+    }
+
+
+
+
+}
