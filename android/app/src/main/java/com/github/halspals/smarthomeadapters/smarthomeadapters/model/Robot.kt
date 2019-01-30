b022dade94c357d1faf29b28c7a99d9008aace62
Ben Sheffield <bshefb@gmail.com>
2019-01-30 04:19:39 +0000

    docs(android): robot model

--- android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/model/Robot.kt
+++ android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/model/Robot.kt
@@ -1,5 +1,11 @@
 package com.github.halspals.smarthomeadapters.smarthomeadapters.model
 
+/**
+ * Model for a smart home adapter robot
+ *
+ * @property nickname name of the robot
+ * @property iconDrawable icon that represents the robot
+ */
 data class Robot(
     val nickname: String,
     val iconDrawable: Int
