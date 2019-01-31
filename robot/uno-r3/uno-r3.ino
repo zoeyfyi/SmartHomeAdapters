edf6593c8e4e1d1f64034818229d8889d359f33a
Ben Sheffield <bshefb@gmail.com>
2019-01-31 20:20:40 +0000

    feat(robot): change LED commands to "led on" and "led off"

--- robot/uno-r3/uno-r3.ino
+++ robot/uno-r3/uno-r3.ino
@@ -12,12 +12,22 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   if(Serial.available() > 0){
-      // turn on when we receive a 1, and off when we receive a 0
-      int value = Serial.read();
-      if(value == '1') {
-          digitalWrite(LED_BUILTIN, HIGH);
-      } else if (value == '0') {
-          digitalWrite(LED_BUILTIN, LOW);
+      String command = Serial.readString();
+      command.remove(command.length() - 1); // remove new line
+
+      Serial.print("Received command: ");
+      Serial.print(command);
+      Serial.println();
+
+      // process LED commands
+      if (command.startsWith("led")) {
+        if(command == "led on") {
+            Serial.println("Turning LED on");
+            digitalWrite(LED_BUILTIN, HIGH);
+        } else if (command == "led off") {
+            Serial.println("Turning LED off");
+            digitalWrite(LED_BUILTIN, LOW);
+        }
       }
   }
 }
