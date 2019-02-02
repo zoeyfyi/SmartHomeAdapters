bb86a385630be0e5e43fd7729cd8220175c1ad04
Gwion ap Rheinallt <gwion.lly@gmail.com>
2019-02-02 20:45:34 +0000

    test(info server):basic unit tests for /robots and /robot/{robotId}

--- infoserver/go.mod
+++ infoserver/go.mod
@@ -2,6 +2,6 @@ module github.com/mrbenshef/SmartHomeAdapters/clientserver
 
 require (
 	github.com/gorilla/mux v1.7.0 // indirect
-	github.com/julienschmidt/httprouter v1.2.0 // indirect
-	github.com/lib/pq v1.0.0 // indirect
+	github.com/julienschmidt/httprouter v1.2.0
+	github.com/lib/pq v1.0.0
 )
--- infoserver/main_test.go
+++ infoserver/main_test.go
@@ -4,24 +4,98 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"strings"
 )
+type testServer struct {
+	Handler http.Handler
+	Server  *httptest.Server
+	URL     string
+}
+func newServer(t *testing.T) *testServer {
+	var server testServer
+	db := getDb()
+	server.Handler = createRouter(db)
+	server.Server = httptest.NewServer(server.Handler)
+	server.URL = server.Server.URL
+	return &server
+}
 
-func TestPing(t *testing.T) {
-	req, err := http.NewRequest("GET", "/ping", nil)
+
+func TestRobots(t *testing.T) {
+
+	responseSubset := "[{\"id\":\"123abc\",\"nickname\":\"testLightbot\",\"robotType\":\"switch\",\"interface\":" +
+		"{\"type\":\"toggle\"}},{\"id\":\"T2D2\",\"nickname\":\"testThermoBot\",\"robotType\":" +
+		"\"thermostat\",\"interface\":{\"type\":\"range\",\"min\":0,\"max\":100}}]"
+
+	s := newServer(t)
+	defer s.Server.Close()
+
+	req, err := http.NewRequest("GET", s.URL + "/robots", nil)
 
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
-
 	rr := httptest.NewRecorder()
 
-	http.HandlerFunc(pingHandler).ServeHTTP(rr, req)
+	s.Handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("Status code differs. Expected \"%d\", Got \"%d\"", http.StatusOK, status)
 	}
 
-	if rr.Body.String() != "pong" {
-		t.Errorf("Body differs. Expected \"%s\", Got: \"%s\"", "pong", rr.Body.String())
+	if !strings.Contains(rr.Body.String(), responseSubset)	{
+		t.Errorf("Body differs. Expected at least \"%s\", Got: \"%s\"", responseSubset, rr.Body.String())
 	}
+}
+
+func TestValidRobotId(t *testing.T) {
+
+	responseSubset := "{\"id\":\"123abc\",\"nickname\":\"testLightbot\",\"robotType\":\"switch\",\"interface\":" +
+		"{\"type\":\"toggle\"}}"
+
+	s := newServer(t)
+	defer s.Server.Close()
+
+	req, err := http.NewRequest("GET", s.URL + "/robot/123abc", nil)
+
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	rr := httptest.NewRecorder()
+
+	s.Handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Status code differs. Expected \"%d\", Got \"%d\"", http.StatusOK, status)
+	}
+
+	if !strings.Contains(rr.Body.String(), responseSubset)	{
+		t.Errorf("Body differs. Expected at least \"%s\", Got: \"%s\"", responseSubset, rr.Body.String())
+	}
+}
+
+func TestInValidRobotId(t *testing.T) {
+
+	response := "\"No robot with that ID\""
+
+	s := newServer(t)
+	defer s.Server.Close()
+
+	req, err := http.NewRequest("GET", s.URL + "/robot/definitelynotalegitrobot", nil)
+
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	rr := httptest.NewRecorder()
+
+	s.Handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Status code differs. Expected \"%d\", Got \"%d\"", http.StatusOK, status)
+	}
+
+	if !strings.Contains(rr.Body.String(), response)	{
+		t.Errorf("Body differs. Expected at least \"%s\", Got: \"%s\"", response, rr.Body.String())
+	}
+
 }
