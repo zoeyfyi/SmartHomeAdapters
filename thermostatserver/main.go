189b8218ee3194528198c734c55fc7b81aa37247
Ben Sheffield <bshefb@gmail.com>
2019-02-15 23:08:32 +0000

    feat(thermostat server): skeleton server

---
+++ thermostatserver/main.go
@@ -0,0 +1,76 @@
+//go:generate protoc --go_out=plugins=grpc:. ./thermostatserver/thermostatserver.proto
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/mrbenshef/SmartHomeAdapters/thermostatserver/thermostatserver"
+	"google.golang.org/grpc"
+)
+
+var (
+	username = os.Getenv("DB_USERNAME")
+	password = os.Getenv("DB_PASSWORD")
+	database = os.Getenv("DB_DATABASE")
+	url      = os.Getenv("DB_URL")
+)
+
+type server struct {
+	DB *sql.DB
+}
+
+func connectionStr() string {
+	if username == "" {
+		username = "postgres"
+	}
+	if password == "" {
+		password = "password"
+	}
+	if url == "" {
+		url = "localhost:5432"
+	}
+	if database == "" {
+		database = "postgres"
+	}
+
+	return fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", username, password, url, database)
+}
+
+func getDb() *sql.DB {
+	log.Printf("Connecting to database with \"%s\"\n", connectionStr())
+	db, err := sql.Open("postgres", connectionStr())
+	if err != nil {
+		log.Fatalf("Failed to connect to postgres: %v", err)
+	}
+
+	return db
+}
+
+func main() {
+	// connect to database
+	db := getDb()
+	defer db.Close()
+
+	// test connection
+	err := db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping postgres: %v", err)
+	}
+
+	log.Printf("Connected to database: %+v\n", db.Stats())
+
+	// start grpc server
+	grpcServer := grpc.NewServer()
+	thermostatServer := &server{DB: db}
+	thermostatserver.RegisterThermostatServerServer(grpcServer, thermostatServer)
+	lis, err := net.Listen("tcp", ":80")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	grpcServer.Serve(lis)
+}
