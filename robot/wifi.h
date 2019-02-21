941ef24278d2ac8134129745acbee4f2ff6f51fc
Xiaobin <wxb9585@gmail.com>
2019-02-21 19:37:57 +0000

    feat(robot): add eduroam wifi connection

--- robot/wifi.h
+++ robot/wifi.h
@@ -1,3 +1,6 @@
 // The SSID and password of the network it should connect to
-#define ssid "YOUR_SSID"  
-#define password "YOUR_PASSWORD"
+#define SSID   "Your SSID"
+#define PASSWORD ""
+#define WPA2_USERNAME "USERNAME for eduroam"
+#define WPA2_IDENTITY WPA2_USERNAME
+#define WPA2_PASSWORD "PASSWORD for eduroam"
