9edc0956bdbc1748bf2b630324df0a0ee31cb4be
Ben Sheffield <bshefb@gmail.com>
2019-02-23 17:01:29 +0000

    feat(account app): simple login/register form

--- account-app/src/App.js
+++ account-app/src/App.js
@@ -1,26 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { css } from 'styled-components'
+
+const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const Input = styled.input`
+  border: 1px solid lightgrey;
+  border-radius: 8px;
+  padding: 8px;
+  font-size: 12pt;
+  width: 250px;
+`;
+
+const Form = styled.form``;
+
+const FormElement = styled.div`
+  margin: 16px;
+  flex: 1;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 8px;
+  border-radius: 8px;
+  background: lightblue;
+  color: white;
+  font-size: 12pt;
+  border: 2px solid lightblue;
+
+  ${props => props.secondary &&
+    css`
+      background: white;
+      color: lightblue;
+    `};
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppContainer>
+        <Form>
+          <Title>HalsPals</Title>
+
+          <FormElement>
+            <Input id="email" type="email" placeholder="john@email.com"/>
+          </FormElement>
+
+          <FormElement>
+            <Input id="password" type="password" placeholder="password"/>
+          </FormElement>
+          
+          <FormElement>
+            <Button>Login</Button>
+          </FormElement>
+          
+          <FormElement>
+            <Button secondary>Register</Button>
+          </FormElement>
+        </Form>
+      </AppContainer>
     );
   }
 }
