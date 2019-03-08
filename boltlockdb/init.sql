11d759bf0b31f088dbf6121933db5236cea4053d
Gwion ap Rheinallt <gwion.lly@gmail.com>
2019-03-08 17:37:38 +0000

    feat(boltlockserver: add boltlock db with test robot)

---
+++ boltlockdb/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE switches (
+    -- ID of the robot
+    serial text not null unique,
+    -- Weather the switch is on or off
+    isOn boolean not null,
+    -- Angles to set servo too
+    onAngle int not null,
+    offAngle int not null,
+    -- Weather the robot has been calibrated
+    isCalibrated boolean not null
+);
+
+INSERT INTO boltlocks (serial, isOn, onAngle, offAngle, isCalibrated) VALUES ('bolty', false, 90, 0, true);
