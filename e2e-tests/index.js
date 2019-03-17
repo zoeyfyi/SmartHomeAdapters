5548aca26d5be338a7222c6e506417553723ef6a
Ben Sheffield <bshefb@gmail.com>
2019-03-17 08:01:58 +0000

    feat(e2e-tests): bring up containers before test suite begins

--- e2e-tests/index.js
+++ e2e-tests/index.js
@@ -1,4 +1,19 @@
-var chakram = require('chakram');
+var chakram = require('chakram'), expect = chakram.expect;
+var compose = require('@terascope/docker-compose-js')("../docker-compose.yml");
+var path = require('path');
+
+before(async () => {
+    // bring up containers
+    await compose.down();
+    await compose.rm(null, { "-v": null }); // remove volumes
+    await compose.up();
+    console.log(await compose.ps());
+})
+
+after(async () => {
+    // bring down containers
+    await compose.down();
+})
 
 describe("Chakram", function() {
     it("should offer simple HTTP request capabilities", function () {
