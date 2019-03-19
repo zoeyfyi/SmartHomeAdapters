884edfb2dc549d2ef85bab4c4d69e4007c64fef2
Ben Sheffield <bshefb@gmail.com>
2019-03-19 23:17:36 +0000

    commit

--- android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/QRFragment.kt
+++ android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/QRFragment.kt
@@ -25,7 +25,6 @@ class QRFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-
         Log.d(fTag, "[onCreateView] Invoked")
         return inflater.inflate(R.layout.fragment_qr, container, false)
     }
