fca71c11bfdb076acee47480effd48443ac1f3cc
TO <theoxolausson@gmail.com>
2019-03-25 15:57:08 +0000

    feat: fragment for robot nickname entry

---
+++ android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/NicknameFragment.kt
@@ -0,0 +1,42 @@
+package com.github.halspals.smarthomeadapters.smarthomeadapters
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_nickname.*
+import kotlinx.android.synthetic.main.view_robot_card.*
+
+/**
+ * A fragment which upon a button click starts a QR scanner from the parent activity.
+ * This is the first screen in the registration wizard.
+ */
+class NicknameFragment : Fragment() {
+
+    private val fTag = "QRFragment"
+
+    private val parent by lazy { activity as RegisterRobotActivity }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        Log.d(fTag, "[onCreateView] Invoked")
+        return inflater.inflate(R.layout.fragment_nickname, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        continue_button.setOnClickListener { _ ->
+            parent.robotNickname = robot_nickname_text_view.toString()
+            parent.startFragment(SelectUseCaseFragment())
+        }
+
+        cancel_button.setOnClickListener { _ -> parent.finish() }
+
+    }
+}
