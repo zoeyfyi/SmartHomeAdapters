a737255182c39f73f666dbea087ea1574a2dcfce
TO <theoxolausson@gmail.com>
2019-04-01 19:53:05 +0100

    fix: updated UseCase model to match server

--- android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/model/UseCase.kt
+++ android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/model/UseCase.kt
@@ -5,15 +5,12 @@ import com.google.gson.annotations.SerializedName
 /**
  * Model for the use case / attachment of a robot
  *
- * @property id the identifier of the use case
  * @property name the use case's common name
  * @property parameters the [ConfigParameter]s associated with the use case
  */
 data class UseCase(
-    @SerializedName("id")
-    val id: String,
     @SerializedName("name")
     val name: String,
-    @SerializedName("parameters")
-    val parameters: List<ConfigParameter>
+    @SerializedName("description")
+    val description: String
 )
