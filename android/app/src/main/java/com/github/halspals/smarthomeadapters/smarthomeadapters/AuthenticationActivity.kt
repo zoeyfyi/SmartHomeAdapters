fcadb4bd12b6d1bc36b249abde6a35c0575a2c66
TO <theoxolausson@gmail.com>
2019-04-01 17:31:03 +0100

    fix: new task on stack when logging in

--- android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/AuthenticationActivity.kt
+++ android/app/src/main/java/com/github/halspals/smarthomeadapters/smarthomeadapters/AuthenticationActivity.kt
@@ -105,7 +105,7 @@ class AuthenticationActivity : AppCompatActivity() {
             writeAuthState(this, authState)
             if (tokenResponse != null) {
                 Log.d(tag, "[exchangeCodeForTokens] Exchange successful, moving to main")
-                startActivity<MainActivity>()
+                startActivity(intentFor<MainActivity>().newTask())
             } else {
                 Log.e(tag, "[exchangeCodeForTokens] Failed, exception $ex")
                 // TODO make the user try again etc
